5c32b5daf646d4cb9dc3bae6941e7a485b61fc56
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-07 13:24:08 +0530

    store value of name, age and is student

--- JavaScript/basic.js
+++ JavaScript/basic.js
@@ -1,3 +1,13 @@
-let a= 10
-let b= 13
-console.log(a>b);
+// let a= 10
+// let b= 13
+// console.log(a>b);
+
+// Create variables to store your name, age, and whether you are a student.
+// let name = "irfan"
+// let age = 22
+// let student = true
+// console.log(name);
+// console.log(age);
+// console.log(student);
+
+
