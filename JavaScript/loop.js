98466edc56f220201951a8166c16c2a087b9fd7d
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-08 16:21:25 +0530

    solve 10 questions

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -12,3 +12,82 @@
 //     }
 // }
 
+// Print numbers from 100 to 1 (reverse order)
+// for(let i = 100; i>0; i--){
+//     console.log(i);
+    
+// }
+
+// Print the multiplication table of 7
+// let num = 7
+// for(let i=1; i<=10; i++){
+//     console.log("Multiplication of", num, "X",i, "=" , i*num);
+// }
+
+// Count how many numbers between 1–100 are divisible by 3
+// let count = 0
+// for(let i=1;i<100;i++){
+//     if(i%3==0){
+//         count++
+//     }    
+// }
+// console.log(count);
+
+
+// Print all numbers between 1–50 that are divisible by both 3 and 5
+// for(let i = 1; i<50; i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(i);        
+//     }
+// }
+
+// Print all odd numbers from 1 to 100
+// for(let i=1; i<100; i++){
+//     if(i%2!=0){
+//         console.log(i);
+        
+//     }
+// }
+
+// // Print numbers from 1–100 but replace:
+// multiples of 3 → "Fizz"
+// multiples of 5 → "Buzz"
+// both → "FizzBuzz"
+
+// for(let i=1; i<100; i++){
+//     if(i%3==0 && i%5==0){
+//         console.log("Fizz",i);
+//     }
+//     else if(i%5==0){
+//         console.log("Buzz",i);
+        
+//     }
+//     else if(i%3==0){
+//         console.log("FizzBuzz",i);
+        
+//     }
+//     else{
+//         console.log("not divisible by 3 and 5",i);
+        
+//     }
+// }
+
+// Find the sum of numbers from 1 to 100
+// let sum =0 
+// for (let i = 1; i < 100; i++){
+//     sum+=i
+//     console.log(sum);
+    
+// }
+
+// Find the factorial of a number (e.g., 5 → 120)
+// for(let i=0; i<=5;i++){
+//     let fact = 1
+//     for(let j=1; j<=i; i++)
+//     {
+//         fact*=i
+//     }
+//     console.log(fact);
+    
+// }
+
