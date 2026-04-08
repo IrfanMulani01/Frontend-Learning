5b6e60a20063f097f85399a00283dd10ed6eb103
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-08 15:27:46 +0530

    print even numbers from 1 to 100

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -3,3 +3,12 @@
 // {
 //     console.log(i);
 // }
+
+// Print all even numbers between 1 and 100
+// for(let i = 0; i<100; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+        
+//     }
+// }
+
