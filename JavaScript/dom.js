fbe874f90b0d794fbb25c607194c225418f8190c
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-10 13:10:52 +0530

    change image

---
+++ JavaScript/dom.js
@@ -0,0 +1,60 @@
+// // Change Text on Button Click
+// Create a button.
+// When clicked, change a paragraph text to "Hello Irfan!".
+// document.getElementById('btn').addEventListener("click", function(){
+//     document.getElementById('text').innerText = "Hello Irfan";
+// });
+
+
+// // Toggle Dark Mode 🌙
+// Add a button.
+// On click, toggle a dark class on <body>.
+// document.getElementById('btn').addEventListener("click", function(){
+//     document.body.classList.toggle('dark');
+// });
+
+
+// // 3. Show/Hide Password
+// Input field + button.
+// Toggle input type between "password" and "text".
+// const password = document.getElementById("pass");
+//         const btn = document.getElementById("btn");
+
+//         btn.addEventListener("click", function () {
+//             if (password.type === "password") {
+//                 password.type = "text";
+//                 btn.innerText = "Hide";
+//             } else {
+//                 password.type = "password";
+//                 btn.innerText = "Show";
+//             }
+//         });
+
+
+// // // 4. Counter App
+// // Buttons: +, -, Reset
+// // Update number dynamically.
+// let count = 0;
+// const number = document.getElementById("num");
+// const plus = document.getElementById("p");
+// const minus = document.getElementById("m");
+//     plus.addEventListener("click", () =>{
+//         count++;
+//         number.innerText=count;
+//     });
+
+//     minus.addEventListener("click", () =>{
+//         count--;
+//         number.innerText=count;
+//     });
+
+
+// 5. Change Image
+// On button click, change image src.
+// const img = document.getElementById("image");
+
+//         document.getElementById("changeBtn").addEventListener("click", function () {
+//             img.src = "https://pixabay.com/images/search/white/";
+//         });
+
+    
